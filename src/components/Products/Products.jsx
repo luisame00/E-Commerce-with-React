a38bd8d284159837ebc084ef51a375ee54df931b
2003480LM <134742988+2003480LM@users.noreply.github.com>
2025-02-25 21:50:39 -0500

    updating active link

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -6,22 +6,22 @@ import { faHeart, faCartPlus } from '@fortawesome/free-solid-svg-icons'
 import './products.css'
 
 const clothes = [
-    { id:1, name: "White T-shirt", price: '$18', img: '/src/components/Products/img/img-1.png', color: 'White', size: 'S', type: 'T-shirt'},
-    { id: 2, name: "Grey Sweater", price: '$22', img: '/src/components/Products/img/img-2.png', color: 'Grey', size: 'M', type: 'Sweater' },
-    { id: 3, name: "White Hoddie", price: '$19', img: '/src/components/Products/img/img-3.png', color: 'White', size: 'L', type: 'Hoodie' },
-    { id: 4, name: "Beige Hoodie", price: '$16', img: '/src/components/Products/img/img-4.png', color: 'Beige', size: 'XL', type: 'Hoodie' },
-    { id: 5, name: "Black Hoodie", price: '$21', img: '/src/components/Products/img/img-5.png', color: 'Black', size: 'S', type: 'Hoodie' },
-    { id: 6, name: "Black Hoodie Nope", price: '$18', img: '/src/components/Products/img/img-6.png', color: 'Black', size: 'M', type: 'Hoodie' },
-    { id: 7, name: "White Hoddie Let ALL", price: '$20', img: '/src/components/Products/img/img-7.png', color: 'White', size: 'S', type: 'Hoodie' },
-    { id: 8, name: "Black Hoddie Be Kind", price: '$24', img: '/src/components/Products/img/img-8.png', color: 'Black', size: 'XL', type: 'Hoodie' },
-    { id: 9, name: "Black t-shirt", price: '$24', img: '/src/components/Products/img/img-9.png', color: 'Black', size: 'L', type: 'T-shirt' },
-    { id: 10, name: "Black t-shirt Symptoms", price: '$16', img: '/src/components/Products/img/img-10.png', color: 'Black', size: 'M', type: 'T-shirt' },
-    { id: 11, name: "White T-shirt Fight", price: '$28', img: '/src/components/Products/img/img-11.png', color: 'White', size: 'S', type: 'T-shirt' },
-    { id: 12, name: "White T-shirt Paris", price: '$23', img: '/src/components/Products/img/img-12.png', color: 'White', size: 'M', type: 'T-shirt' },
-    { id: 13, name: "White T-shirt We Free", price: '$19', img: '/src/components/Products/img/img-13.png', color: 'White', size: 'L', type: 'T-shirt' },
-    { id: 14, name: "Black T-shirt Girl", price: '$29', img: '/src/components/Products/img/img-14.png', color: 'Black', size: 'S', type: 'T-shirt' },
-    { id: 15, name: "White T-shirt Girl", price: '$29', img: '/src/components/Products/img/img-15.png', color: 'White', size: 'XL', type: 'T-shirt' },
-    { id: 16, name: "White T-shirt Relax", price: '$24', img: '/src/components/Products/img/img-16.png', color: 'White', size: 'M', type: 'T-shirt' },
+    { id: 1, name: "White T-shirt", price: '$18', img: './img/img-1.png', color: 'White', size: 'S', type: 'T-shirt' },
+    { id: 2, name: "Grey Sweater", price: '$22', img: './img/img-2.png', color: 'Grey', size: 'M', type: 'Sweater' },
+    { id: 3, name: "White Hoddie", price: '$19', img: './img/img-3.png', color: 'White', size: 'L', type: 'Hoodie' },
+    { id: 4, name: "Beige Hoodie", price: '$16', img: './img/img-4.png', color: 'Beige', size: 'XL', type: 'Hoodie' },
+    { id: 5, name: "Black Hoodie", price: '$21', img: './img/img-5.png', color: 'Black', size: 'S', type: 'Hoodie' },
+    { id: 6, name: "Black Hoodie Nope", price: '$18', img: './img/img-6.png', color: 'Black', size: 'M', type: 'Hoodie' },
+    { id: 7, name: "White Hoddie Let ALL", price: '$20', img: './img/img-7.png', color: 'White', size: 'S', type: 'Hoodie' },
+    { id: 8, name: "Black Hoddie Be Kind", price: '$24', img: './img/img-8.png', color: 'Black', size: 'XL', type: 'Hoodie' },
+    { id: 9, name: "Black t-shirt", price: '$24', img: './img/img-9.png', color: 'Black', size: 'L', type: 'T-shirt' },
+    { id: 10, name: "Black t-shirt Symptoms", price: '$16', img: './img/img-10.png', color: 'Black', size: 'M', type: 'T-shirt' },
+    { id: 11, name: "White T-shirt Fight", price: '$28', img: './img/img-11.png', color: 'White', size: 'S', type: 'T-shirt' },
+    { id: 12, name: "White T-shirt Paris", price: '$23', img: './img/img-12.png', color: 'White', size: 'M', type: 'T-shirt' },
+    { id: 13, name: "White T-shirt We Free", price: '$19', img: './img/img-13.png', color: 'White', size: 'L', type: 'T-shirt' },
+    { id: 14, name: "Black T-shirt Girl", price: '$29', img: './img/img-14.png', color: 'Black', size: 'S', type: 'T-shirt' },
+    { id: 15, name: "White T-shirt Girl", price: '$29', img: './img/img-15.png', color: 'White', size: 'XL', type: 'T-shirt' },
+    { id: 16, name: "White T-shirt Relax", price: '$24', img: './img/img-16.png', color: 'White', size: 'M', type: 'T-shirt' },
 ]
 
 const Products = ({ cart, addToCart, filters = { maxPrice: '', size: '', color: '', type: '' } }) => {
@@ -30,7 +30,7 @@ const Products = ({ cart, addToCart, filters = { maxPrice: '', size: '', color: 
 
     const filteredClothes = clothes.filter(item => {
         const price = parseInt(item.price.replace('$', ''));
-        return(
+        return (
             (!filters.maxPrice || price <= parseInt(filters.maxPrice)) &&
             (!filters.size || item.size.includes(filters.size)) &&
             (!filters.color || item.color.includes(filters.color)) &&
@@ -55,24 +55,24 @@ const Products = ({ cart, addToCart, filters = { maxPrice: '', size: '', color: 
                                 <Card.Title>{item.name}</Card.Title>
                                 <Card.Text>{item.price} </Card.Text>
                                 <Card.Footer>Size: {item.size}</Card.Footer>
-                                <Button 
+                                <Button
                                     variant='light'
                                     onClick={() => addToCart(item)}
                                 >
-                                        <FontAwesomeIcon icon={faCartPlus} size='2x'/>
-                                
+                                    <FontAwesomeIcon icon={faCartPlus} size='2x' />
+
                                 </Button>
 
-                                <Button 
-                                variant="link"
-                                className='heart-button'
-                                onClick={() => toggleLike(item.id)}
+                                <Button
+                                    variant="link"
+                                    className='heart-button'
+                                    onClick={() => toggleLike(item.id)}
                                 >
-                                <FontAwesomeIcon 
-                                    icon={faHeart} 
-                                    size='2x' 
-                                    color={likedProducts.includes(item.id) ? 'red' : 'gray'} 
-                                />
+                                    <FontAwesomeIcon
+                                        icon={faHeart}
+                                        size='2x'
+                                        color={likedProducts.includes(item.id) ? 'red' : 'gray'}
+                                    />
                                 </Button>
                             </Card.Body>
                         </Card>
