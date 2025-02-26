e0a3249cc4f38dbddf25698c97fed9ec8eb27f6a
2003480LM <134742988+2003480LM@users.noreply.github.com>
2025-02-25 21:23:00 -0500

    active link configuration

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://luisame00.github.io/E-Commerce-with-react/',
 })
